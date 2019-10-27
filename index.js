21290a2b34d72438629509c286d0c01b83999532
Chris Zubak-Skees <chriszs@gmail.com>
2019-10-27 13:31:23 -0400

    Better reflect standard Sequelize generated project, structure

--- index.js
+++ index.js
@@ -1,47 +1,5 @@
-const rread = require('readdir-recursive'),
-    path = require('path'),
-    Sequelize = require('sequelize'),
-    sync = require('./sync');
+'use strict';
 
-module.exports = function (options) {
-    let basename = path.basename(module.filename + '/model');
+const models = require('./models');
 
-    let sequelize = new Sequelize(options.name, options.user, options.pass,{
-        host: options.host,
-        dialect: options.driver,
-        port: options.port,
-        define: {
-            createdAt: 'created_date',
-            updatedAt: 'updated_date',
-            underscored: true,
-            timestamps: false
-        },
-        logging: false
-    });
-
-    let db = {};
-
-    rread
-        .fileSync(__dirname + '/model')
-        .filter(file => {
-            file = path.basename(file);
-            return (file.slice(-3) === '.js') && (file !== basename);
-        })
-        .forEach(file => {
-            let model = sequelize['import'](file);
-            db[model.name] = model;
-        });
-
-    Object.keys(db).forEach(modelName => {
-        if (db[modelName].associate) {
-            db[modelName].associate(db);
-        }
-    });
-
-    db.sync = sync.bind(this,options);
-
-    db.sequelize = sequelize;
-    db.Sequelize = Sequelize;
-
-    return db;
-};
+module.exports = models;
---
+++ models/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const sync = require('../sync');
+
+module.exports = function(options) {
+    const basename = path.basename(__filename);
+    const db = {};
+
+    let sequelize = new Sequelize(options.name, options.user, options.pass, {
+        host: options.host,
+        dialect: options.driver,
+        port: options.port,
+        define: {
+            createdAt: 'created_date',
+            updatedAt: 'updated_date',
+            underscored: true,
+            timestamps: false
+        },
+        logging: false
+    });
+
+    fs.readdirSync(__dirname)
+        .filter(file => {
+            return (
+                file.indexOf('.') !== 0 &&
+                file !== basename &&
+                file.slice(-3) === '.js'
+            );
+        })
+        .forEach(file => {
+            const model = sequelize['import'](path.join(__dirname, file));
+            db[model.name] = model;
+        });
+
+    Object.keys(db).forEach(modelName => {
+        if (db[modelName].associate) {
+            db[modelName].associate(db);
+        }
+    });
+
+    db.sync = sync.bind(this, options);
+
+    db.sequelize = sequelize;
+    db.Sequelize = Sequelize;
+
+    return db;
+};
